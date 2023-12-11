814b096680d0cd006551f4d7b4c234b19f2e16a4
scatalfo <scatalfo@gmail.com>
2023-12-11 14:41:50 -0500

    mesibo v1 and sign out button

--- red_thread/lib/main.dart
+++ red_thread/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:mesibo_flutter_sdk/mesibo.dart';
 import 'package:red_thread/amplifyconfiguration.dart';
+import 'package:red_thread/mesiboPage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +32,7 @@ class MainApp extends StatelessWidget {
     return Authenticator(
         child: MaterialApp(
       builder: Authenticator.builder(),
-      home: const HomePage(),
+      home: FirstMesiboApp(),
       theme: ThemeData.from(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSwatch(
@@ -52,28 +54,5 @@ class MainApp extends StatelessWidget {
       // set the theme mode to respond to the user's system preferences (optional)
       themeMode: ThemeMode.system,
     ));
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Red Thread'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Red Thread',
-            ),
-            SignOutButton(),
-          ],
-        ),
-      ),
-    );
   }
 }
