dd31e4cb3683f2fe0474cede8193b5598efcb9b6
scatalfo <scatalfo@gmail.com>
2023-12-11 10:25:50 -0500

    Add Amplify authentication plugins and configure Amplify

--- red_thread/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ red_thread/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,14 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "redthread6ffc13fb": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- red_thread/lib/main.dart
+++ red_thread/lib/main.dart
@@ -1,7 +1,25 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:red_thread/amplifyconfiguration.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureAmplify();
   runApp(const MainApp());
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    // Add Amplify Plugins
+    final amplifyAuthCognito = AmplifyAuthCognito();
+    await Amplify.addPlugins([amplifyAuthCognito]);
+
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    print(e);
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +27,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Authenticator(
+        child: MaterialApp(
+      builder: Authenticator.builder(),
+      home: const HomePage(),
+    ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Red Thread'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to Red Thread',
+            ),
+          ],
         ),
       ),
     );
