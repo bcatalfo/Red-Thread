dceaa4900bb48d4b5049ff9ad78083d715e9490d
Benjamin Catalfo <bcatalfo@gmail.com>
2023-12-12 17:58:16 -0500

    Add AWS CloudFormation provider to project-config.json and configure API authentication in backend-config.json

--- red_thread/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ red_thread/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "redthread": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "redthread": {
       "AppClientID": "string",
