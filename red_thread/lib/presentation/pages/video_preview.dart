7ed5e3b4ef6d378eb859e0872a4a4d56e8e5567a
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-05 18:24:34 -0500

    Added jitsi meet to video preview (WIP)

--- red_thread/lib/presentation/pages/video_preview.dart
+++ red_thread/lib/presentation/pages/video_preview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:red_thread/presentation/drawer_closed.dart';
 import 'package:red_thread/presentation/themes.dart';
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
 
 // make a stateful widget
 class VideoPreview extends StatefulWidget {
@@ -11,6 +12,60 @@ class VideoPreview extends StatefulWidget {
 }
 
 class _VideoPreviewState extends State<VideoPreview> {
+  final jitsiMeet = JitsiMeet();
+
+  void join(){
+    var options = JitsiMeetConferenceOptions(
+      serverURL: "https://jitsi.member.fsf.org",
+      room: "jitsiIsAwesomeWithFlutter",
+      configOverrides: {
+        "startWithAudioMuted": false,
+        "startWithVideoMuted": false,
+        "disableModeratorIndicator": true,
+        "prejoinPageEnabled": false,
+        "breakoutRooms.hideAddRoomButton": true,
+        "breakoutRooms.hideAutoAssignButton": true,
+        "breakoutRooms.hideJoinRoomButton": true,
+        "minHeightForQualityLvl.360": "low",
+        "minHeightForQualityLvl.720": "standard",
+        "minHeightForQualityLvl.1080": "high",
+        "resolution": 1080,
+      },
+      featureFlags: {
+        'unsaferoomwarning.enabled': false,
+        'add-people.enabled': false,
+        'filmstrip.enabled': true,
+        'breakout-rooms.enabled': false,
+        'calendar.enabled': false,
+        'call-integration.enabled': false,
+        'android.screensharing.enabled': false,
+        'live-streaming.enabled': false,
+        'car-mode.enabled': false,
+        'kick-out.enabled': false,
+        'chat.enabled': false,
+        'invite.enabled': false,
+        'meeting-name.enabled': false,
+        'raise-hand.enabled': false,
+        'recording.enabled': false,
+        'server-url-change.enabled': false,
+        'tile-view.enabled': false,
+        'toolbox.alwaysVisible': false,
+        'video-share.enabled': false,
+        'welcomepage.enabled': false,
+        'lobby-mode.enabled': false,
+      },
+      userInfo:
+          JitsiMeetUserInfo(displayName: "Ben", email: 'ben@catalfotechnologies.com'),
+    );
+    jitsiMeet.join(options);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    join();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
