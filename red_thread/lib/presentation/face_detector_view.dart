24e4461d5ccc30593a5595f7e1c4158194421682
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-10 15:46:27 -0500

    Add the custom painter back.

--- red_thread/lib/presentation/face_detector_view.dart
+++ red_thread/lib/presentation/face_detector_view.dart
@@ -76,7 +76,7 @@ class _FaceDetectorViewState extends State<FaceDetectorView> {
         inputImage.metadata!.rotation,
         _cameraLensDirection,
       );
-      _customPaint = null;
+      _customPaint = CustomPaint(painter: painter);
     } else {
       String text = 'Faces found: ${faces.length}\n\n';
       for (final face in faces) {
