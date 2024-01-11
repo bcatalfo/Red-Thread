11a7d2d6b4512ed3c873f99728a4726395db2e16
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-10 22:08:17 -0500

    Change the final to const.

--- red_thread/lib/presentation/face_detector_painter.dart
+++ red_thread/lib/presentation/face_detector_painter.dart
@@ -65,8 +65,8 @@ class FaceDetectorPainter extends CustomPainter {
       );
       // draw a rectangle the same size as the face but centered in the frame
       // Define the central box size
-      final double boxWidth = 240; // Example width
-      final double boxHeight = 320; // Example height
+      const double boxWidth = 240; // Example width
+      const double boxHeight = 320; // Example height
 
       // Calculate the position of the box
       final double left2 = (size.width - boxWidth) / 2;
