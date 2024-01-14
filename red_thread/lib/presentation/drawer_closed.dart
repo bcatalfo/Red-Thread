850a65ebaad3387c04b3d47f379626b206fd61f3
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-14 18:30:59 -0500

    formatting

--- red_thread/lib/presentation/drawer_closed.dart
+++ red_thread/lib/presentation/drawer_closed.dart
@@ -32,36 +32,36 @@ Widget myDrawer = Drawer(
   ),
 );
 
-
 AppBar myAppBar = AppBar(
-          title: Row(
-            children: [
-              const Spacer(),
-              Container(
-                width: 289,
-                height: 61,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: extendedTheme.surfaceContainerHighest,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Text('Red Thread', style: TextStyle(fontSize: 48, color: theme.colorScheme.primary)),
-              ),
-            ],
-          ),
-          leading: Builder(
-            builder: (BuildContext context) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: IconButton(
-                  iconSize: 64,
-                  icon: const Icon(Icons.menu, size: 40),
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  alignment: Alignment.centerLeft,
-                ),
-              );
-            },
-          ),
-        );
+  title: Row(
+    children: [
+      const Spacer(),
+      Container(
+        width: 289,
+        height: 61,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: extendedTheme.surfaceContainerHighest,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Text('Red Thread',
+            style: TextStyle(fontSize: 48, color: theme.colorScheme.primary)),
+      ),
+    ],
+  ),
+  leading: Builder(
+    builder: (BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 15.0),
+        child: IconButton(
+          iconSize: 64,
+          icon: const Icon(Icons.menu, size: 40),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          alignment: Alignment.centerLeft,
+        ),
+      );
+    },
+  ),
+);
