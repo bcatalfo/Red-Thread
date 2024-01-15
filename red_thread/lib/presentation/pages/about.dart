d213f0705f28ef76f518066f903687168ddc8027
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-14 19:35:44 -0500

    About page looking good now.

--- red_thread/lib/presentation/pages/about.dart
+++ red_thread/lib/presentation/pages/about.dart
@@ -5,9 +5,62 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('About')),
-        body: const Center(
-          child: Text('About Page'),
+      appBar: AppBar(
+          title:
+              Text('About', style: Theme.of(context).textTheme.displayLarge)),
+      body: Column(children: [
+        MyCustomCard(
+          mainText: 'Red Thread is...',
+          subText: 'A dating app that actually gets you dating.',
         ),
-      );
+        MyCustomCard(
+          mainText: 'Not like Tinder',
+          subText: 'No swiping. No profiles. Just dates.',
+        ),
+        MyCustomCard(
+          mainText: 'Who we are',
+          subText: 'Sam and Ben Catalfo of Catalfo Technologies LLC',
+        )
+      ]));
+}
+
+class MyCustomCard extends StatelessWidget {
+  final String mainText;
+  final String subText;
+
+  MyCustomCard({
+    required this.mainText,
+    required this.subText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: theme.colorScheme.surfaceVariant,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                mainText,
+                style: theme.textTheme.displayLarge,
+              ),
+            ),
+            const SizedBox(height: 8.0), // Spacer between texts
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                subText,
+                style: theme.textTheme.displayMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
