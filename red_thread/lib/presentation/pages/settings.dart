9546a5e1fb1ef524c1c317fe9025496f24f00277
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-15 03:59:58 -0500

    Add gender identity to settings.

--- red_thread/lib/presentation/pages/settings.dart
+++ red_thread/lib/presentation/pages/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:red_thread/providers.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -7,12 +9,65 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
+        title:
+            Text('Settings', style: Theme.of(context).textTheme.displayLarge),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25, 8, 8, 8),
+            child: Text('Identify As',
+                style: Theme.of(context).textTheme.displayLarge),
+          ),
+          const Center(child: IdentifyAsButton()),
+        ],
+      ));
+}
+
+class IdentifyAsButton extends ConsumerStatefulWidget {
+  const IdentifyAsButton({Key? key}) : super(key: key);
+
+  @override
+  IdentifyAsButtonState createState() => IdentifyAsButtonState();
+}
+
+class IdentifyAsButtonState extends ConsumerState<IdentifyAsButton> {
+  @override
+  Widget build(BuildContext context) {
+    final gender = ref.watch(identifyAsProvider);
+
+    return Column(children: <Widget>[
+      RadioListTile<Gender>(
+          title: Text('Male', style: Theme.of(context).textTheme.displayMedium),
+          value: Gender.male,
+          groupValue: gender,
+          onChanged: (value) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ref.read(identifyAsProvider.notifier).state = value!;
+            });
+          }),
+      RadioListTile<Gender>(
           title:
-              Text('Settings', style: Theme.of(context).textTheme.displayLarge),
-        ),
-        body: const Center(
-          child: Text('Settings Page'),
-        ),
-      );
+              Text('Female', style: Theme.of(context).textTheme.displayMedium),
+          value: Gender.female,
+          groupValue: gender,
+          onChanged: (value) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ref.read(identifyAsProvider.notifier).state = value!;
+            });
+          }),
+      RadioListTile<Gender>(
+          title:
+              Text('Other', style: Theme.of(context).textTheme.displayMedium),
+          value: Gender.other,
+          groupValue: gender,
+          onChanged: (value) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ref.read(identifyAsProvider.notifier).state = value!;
+            });
+          }),
+    ]);
+  }
 }
--- red_thread/lib/providers.dart
+++ red_thread/lib/providers.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+enum Gender { male, female, other }
+
 // TODO: Get this from AWS
 final matchFoundProvider = StateProvider<bool>((ref) => false);
+final identifyAsProvider = StateProvider<Gender>((ref) => Gender.male);
