02b71de58817f2ac21cda8881feb509c99d94731
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-16 00:59:38 -0500

    Killed Jitsi.

--- red_thread/lib/main.dart
+++ red_thread/lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import 'package:flutter/services.dart';
-import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
-import 'package:red_thread/presentation/pages/preview.dart';
 import 'package:red_thread/router.dart';
 import 'package:red_thread/presentation/themes.dart';
-import 'package:red_thread/jitsi_meet_options.dart';
-import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,32 +15,8 @@ void main() async {
 class BagoolApp extends ConsumerWidget {
   const BagoolApp({super.key});
 
-  static final jitsiMeet = JitsiMeet();
-
   static Future<void> join(BuildContext context, WidgetRef ref) async {
-    final listener = JitsiMeetEventListener(
-      conferenceJoined: (url) {
-        debugPrint("conferenceJoined: url: $url");
-        ref.read(isJoiningProvider.notifier).state = false;
-        context.go('/chat');
-      },
-      participantJoined: (email, name, role, participantId) {
-        debugPrint(
-          "participantJoined: email: $email, name: $name, role: $role, "
-          "participantId: $participantId",
-        );
-      },
-      chatMessageReceived: (senderId, message, isPrivate, timestamp) {
-        debugPrint(
-          "chatMessageReceived: senderId: $senderId, message: $message, "
-          "isPrivate: $isPrivate timestamp: $timestamp",
-        );
-      },
-      readyToClose: () {
-        debugPrint("readyToClose");
-      },
-    );
-    await jitsiMeet.join(options, listener);
+    debugPrint('ur mom');
   }
 
   @override
