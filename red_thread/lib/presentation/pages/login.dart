ae1a6b8fce49828ae147fa933d41bb5a93cedaa5
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-22 20:53:34 -0500

    Added login button.

--- red_thread/lib/presentation/pages/login.dart
+++ red_thread/lib/presentation/pages/login.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import 'package:red_thread/providers.dart';
 
 class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,12 +15,23 @@ class LoginPageState extends ConsumerState<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
-      body: const Center(
-        child: Text("Login Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Login"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text("Login Page"),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(isAuthenticatedProvider.notifier).state = true;
+                  //context.go("/account_setup");
+                },
+                child: const Text("Go to Account Setup"),
+              ),
+            ],
+          ),
+        ));
   }
 }
