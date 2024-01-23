a3aed6978623d0878e31815f72101723050e96aa
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-22 21:02:43 -0500

    Added button to finish account setup.

--- red_thread/lib/presentation/pages/account_setup.dart
+++ red_thread/lib/presentation/pages/account_setup.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import 'package:red_thread/providers.dart';
 
 class AccountSetupPage extends ConsumerStatefulWidget {
   const AccountSetupPage({Key? key}) : super(key: key);
@@ -14,12 +15,24 @@ class AccountSetupPageState extends ConsumerState<AccountSetupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Account Setup"),
-      ),
-      body: const Center(
-        child: Text("Account Setup Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Account Setup"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text("Account Setup Page"),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(isAccountSetupCompleteProvider.notifier).state =
+                      true;
+                  //context.go("/account_setup");
+                },
+                child: const Text("Go to verification"),
+              ),
+            ],
+          ),
+        ));
   }
 }
