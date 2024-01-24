94ef07bae5747ab1c40cba7c590024961d6e4823
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-23 21:43:12 -0500

    Welcome page is now slightly less shitty.

--- red_thread/lib/presentation/pages/welcome.dart
+++ red_thread/lib/presentation/pages/welcome.dart
@@ -12,23 +12,47 @@ class WelcomePage extends ConsumerStatefulWidget {
 class WelcomePageState extends ConsumerState<WelcomePage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const Text("Welcome Page"),
-              ElevatedButton(
-                onPressed: () {
-                  ref.read(isFirstTimeUserProvider.notifier).state = false;
-                },
-                child: const Text("Go to Login"),
+      appBar: AppBar(
+        title: const Text("Welcome"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Welcome to Red Thread",
+                style: theme.textTheme.headlineLarge,
               ),
-            ],
-          ),
-        ));
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/images/iTunesArtwork-1024.png', // Replace with your image path
+              width: 200, // Adjust the size as needed
+              height: 200,
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Text(
+                "Find your thread of connection",
+                style: theme.textTheme.bodyLarge,
+              ),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(isFirstTimeUserProvider.notifier).state = false;
+              },
+              child: const Text("Go to Login"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
