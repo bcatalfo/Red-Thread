ede564170277470133c5a84caa9cdf38dafb9ef3
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-23 21:47:12 -0500

    Added username and password fields to login page.

--- red_thread/lib/providers.dart
+++ red_thread/lib/providers.dart
@@ -6,6 +6,7 @@ enum Gender { male, female, other }
 // TODO: Get this from backend
 final matchFoundProvider = StateProvider<bool>((ref) => false);
 final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.light);
+final isFirstTimeUserProvider = StateProvider<bool>((ref) => true);
 final isAuthenticatedProvider = StateProvider<bool>((ref) => false);
 final isAccountSetupCompleteProvider = StateProvider<bool>((ref) => false);
 final isVerifiedProvider = StateProvider<bool>((ref) => false);
