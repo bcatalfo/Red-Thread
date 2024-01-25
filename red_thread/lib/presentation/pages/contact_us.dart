03909547c06a2ca8f137dae6ef05be4dbcc83485
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-24 21:12:50 -0500

    Added calls and emails to the Contact Us page.

--- red_thread/lib/presentation/pages/contact_us.dart
+++ red_thread/lib/presentation/pages/contact_us.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:url_launcher/url_launcher.dart";
 
 class ContactUsPage extends StatefulWidget {
   const ContactUsPage({Key? key}) : super(key: key);
@@ -10,6 +11,33 @@ class ContactUsPage extends StatefulWidget {
 }
 
 class ContactUsPageState extends State<ContactUsPage> {
+  void _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    if (await canLaunchUrl(launchUri)) {
+      await launchUrl(launchUri);
+    } else {
+      // Handle the situation when the phone call cannot be made
+      debugPrint('Could not launch $launchUri');
+    }
+  }
+
+  void _sendEmail(String emailAddress1, String emailAddress2) async {
+    final Uri launchUri = Uri(
+      scheme: 'mailto',
+      path:
+          '$emailAddress1,$emailAddress2', // Separate email addresses with a comma
+    );
+    if (await canLaunchUrl(launchUri)) {
+      await launchUrl(launchUri);
+    } else {
+      // Handle the situation when the email cannot be sent
+      debugPrint('Could not launch $launchUri');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -17,20 +45,36 @@ class ContactUsPageState extends State<ContactUsPage> {
     return Scaffold(
       appBar: AppBar(
           title: Text("Contact Us", style: theme.textTheme.displayLarge)),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizedBox(height: 40),
-              Text(
-                "Contact Us",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 40),
-              Text("If you have any questions or concerns, please email us at"),
+              Text("Call us anytime", style: theme.textTheme.headlineLarge),
+              const SizedBox(height: 40),
+              ElevatedButton.icon(
+                  onPressed: () {
+                    _makePhoneCall('+1631-560-8030');
+                  },
+                  icon: const Padding(
+                    padding: EdgeInsets.only(right: 16.0),
+                    child: Icon(Icons.phone),
+                  ),
+                  label: Text("Call Us", style: theme.textTheme.bodyLarge)),
+              const SizedBox(height: 40),
+              Text("Or send us an email", style: theme.textTheme.headlineLarge),
+              const SizedBox(height: 40),
+              ElevatedButton.icon(
+                  onPressed: () {
+                    _sendEmail('ben@catalfotechnologies.com',
+                        'sam@catalfotechnologies.com');
+                  },
+                  icon: const Padding(
+                    padding: EdgeInsets.only(right: 16.0),
+                    child: Icon(Icons.email),
+                  ),
+                  label: Text("Email Us", style: theme.textTheme.bodyLarge))
             ],
           ),
         ),
