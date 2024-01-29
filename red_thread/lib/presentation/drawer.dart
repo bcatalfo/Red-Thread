4e553e34ff3537a2be146675cdbb4a62405b179e
Benjamin Catalfo <bcatalfo@gmail.com>
2024-01-29 13:52:08 -0500

    Allowed spacing between the standard buttons and the log out / delete account buttons.

--- red_thread/lib/presentation/drawer.dart
+++ red_thread/lib/presentation/drawer.dart
@@ -11,61 +11,122 @@ Drawer myDrawer(BuildContext context, WidgetRef ref) {
 
   return Drawer(
     backgroundColor: theme.colorScheme.surfaceVariant,
-    child: ListView(
-      padding: const EdgeInsets.all(8.0),
-      children: [
-        DrawerHeader(
-            child: Image.asset('assets/images/iTunesArtwork-1024.png')),
-        ListTile(
-          leading:
-              Icon(Icons.home, size: theme.textTheme.displayMedium?.fontSize),
-          title: Text('Home', style: theme.textTheme.displayMedium),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading:
-              Icon(Icons.phone, size: theme.textTheme.displayMedium?.fontSize),
-          title: Text('Contact Us', style: theme.textTheme.displayMedium),
-          onTap: () {
-            context.push('/contact_us');
-          },
-        ),
-        ListTile(
-          leading:
-              Icon(Icons.info, size: theme.textTheme.displayMedium?.fontSize),
-          title: Text('About', style: theme.textTheme.displayMedium),
-          onTap: () {
-            context.push('/about');
-          },
-        ),
-        themeMode == ThemeMode.light
-            ? ListTile(
-                leading: Icon(Icons.dark_mode,
+    child: LayoutBuilder(
+      builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+        constraints: BoxConstraints(minHeight: constraints.maxHeight),
+        child: IntrinsicHeight(
+          child: Column(
+            children: [
+              DrawerHeader(
+                  child: Image.asset('assets/images/iTunesArtwork-1024.png')),
+              ListTile(
+                leading: Icon(Icons.home,
                     size: theme.textTheme.displayMedium?.fontSize),
-                title: Text('Dark Mode', style: theme.textTheme.displayMedium),
+                title: Text('Home', style: theme.textTheme.displayMedium),
                 onTap: () {
-                  ref.read(themeModeProvider.notifier).state = ThemeMode.dark;
-                })
-            : ListTile(
-                leading: Icon(Icons.light_mode,
-                    size: theme.textTheme.displayMedium?.fontSize),
-                title: Text('Light Mode', style: theme.textTheme.displayMedium),
-                onTap: () {
-                  ref.read(themeModeProvider.notifier).state = ThemeMode.light;
+                  Navigator.pop(context);
                 },
               ),
-        ListTile(
-          leading:
-              Icon(Icons.logout, size: theme.textTheme.displayMedium?.fontSize),
-          title: Text('Log Out', style: theme.textTheme.displayMedium),
-          onTap: () {
-            // TODO: Actually log out
-            ref.read(isAuthenticatedProvider.notifier).state = false;
-          },
-        )
-      ],
+              ListTile(
+                leading: Icon(Icons.phone,
+                    size: theme.textTheme.displayMedium?.fontSize),
+                title: Text('Contact Us', style: theme.textTheme.displayMedium),
+                onTap: () {
+                  context.push('/contact_us');
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.info,
+                    size: theme.textTheme.displayMedium?.fontSize),
+                title: Text('About', style: theme.textTheme.displayMedium),
+                onTap: () {
+                  context.push('/about');
+                },
+              ),
+              themeMode == ThemeMode.light
+                  ? ListTile(
+                      leading: Icon(Icons.dark_mode,
+                          size: theme.textTheme.displayMedium?.fontSize),
+                      title: Text('Dark Mode',
+                          style: theme.textTheme.displayMedium),
+                      onTap: () {
+                        ref.read(themeModeProvider.notifier).state =
+                            ThemeMode.dark;
+                      })
+                  : ListTile(
+                      leading: Icon(Icons.light_mode,
+                          size: theme.textTheme.displayMedium?.fontSize),
+                      title: Text('Light Mode',
+                          style: theme.textTheme.displayMedium),
+                      onTap: () {
+                        ref.read(themeModeProvider.notifier).state =
+                            ThemeMode.light;
+                      },
+                    ),
+              const Spacer(),
+              ListTile(
+                leading: Icon(Icons.logout,
+                    size: theme.textTheme.displayMedium?.fontSize),
+                title: Text('Log Out', style: theme.textTheme.displayMedium),
+                onTap: () {
+                  // TODO: Actually log out
+                  ref.read(isAuthenticatedProvider.notifier).state = false;
+                },
+              ),
+              ListTile(
+                  leading: Icon(Icons.delete,
+                      size: theme.textTheme.displayMedium?.fontSize),
+                  title: FittedBox(
+                      fit: BoxFit.contain,
+                      alignment: Alignment.centerLeft,
+                      child: Text('Delete Account',
+                          style: theme.textTheme.displayMedium)),
+                  onTap: () {
+                    // Make an alert dialog
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        backgroundColor: theme.colorScheme.surfaceVariant,
+                        title: Text('Delete Account?',
+                            style: theme.textTheme.headlineMedium),
+                        content: Text(
+                            'Are you sure you want to delete your account? This action cannot be undone.',
+                            style: theme.textTheme.bodyLarge),
+                        actionsAlignment: MainAxisAlignment.center,
+                        actions: [
+                          ButtonBar(
+                            alignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  ref
+                                      .read(isAuthenticatedProvider.notifier)
+                                      .state = false;
+                                },
+                                child: Text('Delete',
+                                    style: theme.textTheme.bodyLarge?.copyWith(
+                                        color: theme.colorScheme.primary)),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancel',
+                                    style: theme.textTheme.bodyLarge?.copyWith(
+                                        color: theme.colorScheme.primary)),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ],
+          ),
+        ),
+      )),
     ),
   );
 }
