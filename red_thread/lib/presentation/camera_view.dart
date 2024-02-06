5fe33a978e5993f4c12e1e0f2844b693a953cf94
Benjamin Catalfo <bcatalfo@gmail.com>
2024-02-06 17:01:56 -0500

    Changed camera quality to low because the docs say so.

--- red_thread/lib/presentation/camera_view.dart
+++ red_thread/lib/presentation/camera_view.dart
@@ -83,7 +83,7 @@ class _CameraViewState extends State<CameraView> {
     _controller = CameraController(
       camera,
       // Set to ResolutionPreset.high. Do NOT set it to ResolutionPreset.max because for some phones does NOT work.
-      ResolutionPreset.medium,
+      ResolutionPreset.low,
       enableAudio: false,
       imageFormatGroup: Platform.isAndroid
           ? ImageFormatGroup.nv21
