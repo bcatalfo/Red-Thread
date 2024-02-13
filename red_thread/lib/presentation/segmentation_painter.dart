e1707beecda8dc8c19722bd38873f7a10074a319
Benjamin Catalfo <bcatalfo@gmail.com>
2024-02-13 16:30:08 -0500

    Cleaned up the code and removed the opacity.

--- red_thread/lib/presentation/segmentation_painter.dart
+++ red_thread/lib/presentation/segmentation_painter.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-import 'dart:ui' as ui;
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_selfie_segmentation/google_mlkit_selfie_segmentation.dart';
@@ -21,7 +18,6 @@ class SegmentationPainter extends CustomPainter {
   final Color color = globalLightScheme.surfaceVariant;
   final InputImageRotation rotation;
   final CameraLensDirection cameraLensDirection;
-  final Random random = Random();
   List<Rect> rectangles = [];
 
   void calculateRectangles(Size size){
@@ -95,30 +91,13 @@ class SegmentationPainter extends CustomPainter {
         if (confidences[(y * width) + x] > 0.5) {
           offsets.add(Offset(tx.toDouble(), ty.toDouble()));
         }
-
-        //final double opacity = (1 - confidences[(y * width) + x]) * 0.5;
-        //paint.color = color.withOpacity(opacity);
-        //canvas.drawCircle(Offset(tx.toDouble(), ty.toDouble()), 2, paint);
       }
     }
     paint.color = color;
-    //canvas.drawPoints(ui.PointMode.points, offsets, paint);
-    //canvas.drawRawPoints(ui.PointMode.points, offsets, paint);
-    //canvas.drawPicture(picture)
-    // iterate through each rectangle in mask.height, mask.width
-
-    // make offset contain one hundredth of the offsets it originally contained. do this randomly
-    // choose a offsets.length / 100 random offsets to keep
-    
-
-    // Paint setup for blurring
-    //final blurPaint = Paint()
-    //  ..color = globalLightScheme.surfaceVariant.withOpacity(0.85);
-      //..maskFilter = const ui.MaskFilter.blur(ui.BlurStyle.normal, 10.0);
 
     for (var rectangle in rectangles) {
       bool faceInRectangle = false;
-      final paint = Paint()..color = color.withOpacity(0.95)
+      final paint = Paint()..color = color
       ..blendMode = BlendMode.src;
       for (var offset in offsets) {
         if (rectangle.contains(offset)) {
@@ -130,22 +109,6 @@ class SegmentationPainter extends CustomPainter {
         canvas.drawRect(rectangle, paint);
       }
     }
-
-    /* for (var offset in offsets) {
-      canvas.drawCircle(offset, 20, paint);
-      //canvas.drawRect(rrect, paint);
-    } */
-    final paragraphStyle = ui.ParagraphStyle(
-      textDirection: TextDirection.ltr,
-    );
-    final paragraphBuilder = ui.ParagraphBuilder(paragraphStyle)
-      ..pushStyle(ui.TextStyle(color: Colors.black, fontSize: 20))
-      ..addText(
-          'Size of offsets: ${offsets.length} \n Size of rectangles: ${rectangles.length}');
-    final paragraph = paragraphBuilder.build();
-    paragraph.layout(ui.ParagraphConstraints(width: size.width));
-    const offset = Offset(50, 50);
-    canvas.drawParagraph(paragraph, offset);
   }
 
   @override
