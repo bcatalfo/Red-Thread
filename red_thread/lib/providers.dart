468bbaf99e163cb492ba80286b2d7520f330d032
Benjamin Catalfo <bcatalfo@gmail.com>
2024-05-08 23:09:39 -0400

    Fixed all the providers.

--- red_thread/lib/providers.dart
+++ red_thread/lib/providers.dart
@@ -9,15 +9,14 @@ enum DateSchedule { notScheduled, sent, received, confirmed }
 // TODO: Get this from backend
 final matchFoundProvider = StateProvider<bool>((ref) => true);
 final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.light);
-final isFirstTimeUserProvider = StateProvider<bool>((ref) => false);
+final needsWelcomingProvider = StateProvider<bool>((ref) => false);
 final isAuthenticatedProvider = StateProvider<bool>((ref) => true);
 final isAccountSetupCompleteProvider = StateProvider<bool>((ref) => true);
 final isVerifiedProvider = StateProvider<bool>((ref) => false);
 final inQueueProvider = StateProvider<bool>((ref) => false);
-final secsInQueueProvider = StateProvider<int>((ref) => 0);
-final isQueueVisibleProvider = StateProvider<bool>((ref) => false);
-final dateTimeProvider =
-    StateProvider<TimeOfDay?>((ref) => TimeOfDay(hour: 12, minute: 0));
+final whenJoinedQueueProvider = StateProvider<DateTime?>((ref) => null);
+final dateTimeProvider = StateProvider<DateTime?>(
+    (ref) => DateTime.now().add(const Duration(days: 1)));
 final dateLocationProvider = StateProvider<String?>((ref) => "your moms house");
 final dateScheduleProvider =
     StateProvider<DateSchedule>((ref) => DateSchedule.received);
