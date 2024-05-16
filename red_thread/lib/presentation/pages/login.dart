7692f7a0ac801a40ed344a875582468d5a50a143
scatalfo <scatalfo@gmail.com>
2024-05-16 14:50:17 -0400

    login v1

--- red_thread/lib/presentation/pages/login.dart
+++ red_thread/lib/presentation/pages/login.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import 'package:red_thread/providers.dart';
@@ -88,9 +90,26 @@ class LoginPageState extends ConsumerState<LoginPage> {
                 ),
                 const SizedBox(height: 40),
                 ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (_formKey.currentState!.validate()) {
-                      ref.read(isAuthenticatedProvider.notifier).state = true;
+                      try {
+                        final credential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: _usernameController.text,
+                          password: _passwordController.text,
+                        );
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          debugPrint('The password provided is too weak.');
+                        } else if (e.code == 'email-already-in-use') {
+                          debugPrint(
+                              'The account already exists for that email.');
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                      ref.read(isAuthenticatedProvider.notifier).state =
+                          FirebaseAuth.instance.currentUser != null;
                       // Add your authentication logic here
                     }
                   },
