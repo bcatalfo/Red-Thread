606b0eb75c9b5de92af5e2fd7e4e6454014e0cb4
scatalfo <scatalfo@gmail.com>
2024-05-16 20:15:11 -0400

    login v2

--- red_thread/lib/presentation/pages/login.dart
+++ red_thread/lib/presentation/pages/login.dart
@@ -94,16 +94,14 @@ class LoginPageState extends ConsumerState<LoginPage> {
                     if (_formKey.currentState!.validate()) {
                       try {
                         final credential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                          email: _usernameController.text,
-                          password: _passwordController.text,
-                        );
+                            .signInWithEmailAndPassword(
+                                email: _usernameController.text,
+                                password: _passwordController.text);
                       } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak-password') {
-                          debugPrint('The password provided is too weak.');
-                        } else if (e.code == 'email-already-in-use') {
-                          debugPrint(
-                              'The account already exists for that email.');
+                        if (e.code == 'user-not-found') {
+                          print('No user found for that email.');
+                        } else if (e.code == 'wrong-password') {
+                          print('Wrong password provided for that user.');
                         }
                       } catch (e) {
                         print(e);
