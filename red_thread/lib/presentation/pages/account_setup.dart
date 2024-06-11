e2ca6c3ca19907f86aba08c7e9f71ac3372fb32e
Benjamin Catalfo <bcatalfo@gmail.com>
2024-06-11 00:26:12 -0400

    Fixed the formKeys (and by doing so the face verification bugs!)

--- red_thread/lib/presentation/pages/account_setup.dart
+++ red_thread/lib/presentation/pages/account_setup.dart
@@ -481,191 +481,6 @@ class AccountSetupPageState extends ConsumerState<AccountSetupPage>
     );
   }
 
-  Widget _buildEnterPhoneNumberPage(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKeys[11], // Ensure correct key index
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Please enter your phone number",
-                  style: textTheme.headlineMedium,
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: DropdownSearch<Map<String, String>>(
-                        items: _countryCodes,
-                        itemAsString: (Map<String, String> country) =>
-                            "${country['name']} ${country['code']}",
-                        selectedItem: _selectedCountryCode,
-                        dropdownBuilder:
-                            (_, Map<String, String>? selectedItem) {
-                          return Text(selectedItem != null
-                              ? "${selectedItem['name']} ${selectedItem['code']}"
-                              : "Select Country Code");
-                        },
-                        popupProps: PopupProps.modalBottomSheet(
-                          showSearchBox: true,
-                          searchFieldProps: const TextFieldProps(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Search Country',
-                            ),
-                          ),
-                          fit: FlexFit.tight,
-                          itemBuilder: (context, item, isSelected) {
-                            return ListTile(
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(item['name']!),
-                                  Text(item['code']!),
-                                ],
-                              ),
-                            );
-                          },
-                        ),
-                        onChanged: (Map<String, String>? value) {
-                          setState(() {
-                            _selectedCountryCode = value!;
-                          });
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      flex: 5,
-                      child: TextFormField(
-                        controller: _phoneNumberController,
-                        decoration:
-                            const InputDecoration(labelText: "Phone Number"),
-                        keyboardType: TextInputType.phone,
-                        inputFormatters: [
-                          LengthLimitingTextInputFormatter(
-                              12), // Maximum 12 characters including hyphens
-                          FilteringTextInputFormatter.allow(RegExp(r'[0-9-]')),
-                          TextInputFormatter.withFunction((oldValue, newValue) {
-                            final text = newValue.text;
-                            final digitsOnly =
-                                text.replaceAll(RegExp(r'\D'), '');
-                            if (digitsOnly.length > 10) {
-                              return oldValue;
-                            }
-                            final buffer = StringBuffer();
-                            for (int i = 0; i < digitsOnly.length; i++) {
-                              if (i == 3 || i == 6) buffer.write('-');
-                              buffer.write(digitsOnly[i]);
-                            }
-                            return TextEditingValue(
-                              text: buffer.toString(),
-                              selection: TextSelection.collapsed(
-                                  offset: buffer.length),
-                            );
-                          }),
-                        ],
-                        validator: (value) {
-                          final digitsOnly =
-                              value?.replaceAll(RegExp(r'\D'), '');
-                          if (digitsOnly == null || digitsOnly.length != 10) {
-                            return 'Please enter a valid phone number';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          setState(
-                              () {}); // Trigger rebuild to update button state
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "To verify your identity, we will send a code via SMS. Message and data rates may apply. Please be aware.",
-                  style: textTheme.bodyLarge,
-                ),
-              ],
-            ),
-            Positioned(
-              bottom: 48,
-              left: 0,
-              right: 0,
-              child:
-                  _buildNavigationButtons(context, onNext: _phoneVerification),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildEnterSMSCodePage(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKeys[12], // Ensure correct key index
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Please enter the SMS code",
-                  style: textTheme.headlineMedium,
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _smsCodeController,
-                  decoration: const InputDecoration(
-                    labelText: "SMS Code",
-                    hintText: "_ _ _ _ _ _",
-                  ),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(
-                        11), // Maximum 11 characters (6 digits + 5 spaces)
-                    FilteringTextInputFormatter.digitsOnly,
-                    SmsCodeInputFormatter(),
-                  ],
-                  validator: (value) {
-                    final digitsOnly = value?.replaceAll(' ', '');
-                    if (digitsOnly == null || digitsOnly.length != 6) {
-                      return 'Please enter a valid 6-digit code';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    setState(() {}); // Trigger rebuild to update button state
-                  },
-                ),
-              ],
-            ),
-            Positioned(
-              bottom: 48,
-              left: 0,
-              right: 0,
-              child: _buildNavigationButtons(context, onNext: _verifySMSCode),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   Widget _buildFaceVerificationPage(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
@@ -1061,6 +876,234 @@ class AccountSetupPageState extends ConsumerState<AccountSetupPage>
     );
   }
 
+  Widget _buildContactsPage(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKeys[10],
+        autovalidateMode: AutovalidateMode.disabled,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't match with people you know",
+                  style: textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "By sharing your contacts with us, we can make sure that you don't match with people you already know.",
+                  style: textTheme.bodyLarge,
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 48,
+              left: 0,
+              right: 0,
+              child: _buildNavigationButtons(context, onNext: _getContacts),
+            ),
+            Align(
+              alignment: const FractionalOffset(0.5, 0.333),
+              child: Icon(
+                Icons.contact_mail,
+                size: 128,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEnterPhoneNumberPage(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKeys[11],
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Please enter your phone number",
+                  style: textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: DropdownSearch<Map<String, String>>(
+                        items: _countryCodes,
+                        itemAsString: (Map<String, String> country) =>
+                            "${country['name']} ${country['code']}",
+                        selectedItem: _selectedCountryCode,
+                        dropdownBuilder:
+                            (_, Map<String, String>? selectedItem) {
+                          return Text(selectedItem != null
+                              ? "${selectedItem['name']} ${selectedItem['code']}"
+                              : "Select Country Code");
+                        },
+                        popupProps: PopupProps.modalBottomSheet(
+                          showSearchBox: true,
+                          searchFieldProps: const TextFieldProps(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Search Country',
+                            ),
+                          ),
+                          fit: FlexFit.tight,
+                          itemBuilder: (context, item, isSelected) {
+                            return ListTile(
+                              title: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(item['name']!),
+                                  Text(item['code']!),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                        onChanged: (Map<String, String>? value) {
+                          setState(() {
+                            _selectedCountryCode = value!;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 5,
+                      child: TextFormField(
+                        controller: _phoneNumberController,
+                        decoration:
+                            const InputDecoration(labelText: "Phone Number"),
+                        keyboardType: TextInputType.phone,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(
+                              12), // Maximum 12 characters including hyphens
+                          FilteringTextInputFormatter.allow(RegExp(r'[0-9-]')),
+                          TextInputFormatter.withFunction((oldValue, newValue) {
+                            final text = newValue.text;
+                            final digitsOnly =
+                                text.replaceAll(RegExp(r'\D'), '');
+                            if (digitsOnly.length > 10) {
+                              return oldValue;
+                            }
+                            final buffer = StringBuffer();
+                            for (int i = 0; i < digitsOnly.length; i++) {
+                              if (i == 3 || i == 6) buffer.write('-');
+                              buffer.write(digitsOnly[i]);
+                            }
+                            return TextEditingValue(
+                              text: buffer.toString(),
+                              selection: TextSelection.collapsed(
+                                  offset: buffer.length),
+                            );
+                          }),
+                        ],
+                        validator: (value) {
+                          final digitsOnly =
+                              value?.replaceAll(RegExp(r'\D'), '');
+                          if (digitsOnly == null || digitsOnly.length != 10) {
+                            return 'Please enter a valid phone number';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          setState(
+                              () {}); // Trigger rebuild to update button state
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "To verify your identity, we will send a code via SMS. Message and data rates may apply. Please be aware.",
+                  style: textTheme.bodyLarge,
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 48,
+              left: 0,
+              right: 0,
+              child:
+                  _buildNavigationButtons(context, onNext: _phoneVerification),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEnterSMSCodePage(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKeys[12],
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Please enter the SMS code",
+                  style: textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _smsCodeController,
+                  decoration: const InputDecoration(
+                    labelText: "SMS Code",
+                    hintText: "_ _ _ _ _ _",
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(
+                        11), // Maximum 11 characters (6 digits + 5 spaces)
+                    FilteringTextInputFormatter.digitsOnly,
+                    SmsCodeInputFormatter(),
+                  ],
+                  validator: (value) {
+                    final digitsOnly = value?.replaceAll(' ', '');
+                    if (digitsOnly == null || digitsOnly.length != 6) {
+                      return 'Please enter a valid 6-digit code';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {}); // Trigger rebuild to update button state
+                  },
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 48,
+              left: 0,
+              right: 0,
+              child: _buildNavigationButtons(context, onNext: _verifySMSCode),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   Future<void> _phoneVerification() async {
     final completer = Completer<void>();
     await FirebaseAuth.instance.verifyPhoneNumber(
@@ -1157,58 +1200,20 @@ class AccountSetupPageState extends ConsumerState<AccountSetupPage>
     await completer.future;
   }
 
-  Widget _buildContactsPage(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKeys[10],
-        autovalidateMode: AutovalidateMode.disabled,
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't match with people you know",
-                  style: textTheme.headlineMedium,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  "By sharing your contacts with us, we can make sure that you don't match with people you already know.",
-                  style: textTheme.bodyLarge,
-                ),
-              ],
-            ),
-            Positioned(
-              bottom: 48,
-              left: 0,
-              right: 0,
-              child: _buildNavigationButtons(context, onNext: _getContacts),
-            ),
-            Align(
-              alignment: const FractionalOffset(0.5, 0.333),
-              child: Icon(
-                Icons.contact_mail,
-                size: 128,
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   Widget _buildNavigationButtons(BuildContext context,
       {Future<void> Function()? onNext}) {
     bool isValid = _formKeys[_currentStep].currentState?.validate() ?? false;
 
-    if (_currentStep >= 8) {
+    if (_currentStep == 6 ||
+        _currentStep == 7 ||
+        _currentStep == 8 ||
+        _currentStep == 9 ||
+        _currentStep == 10) {
+      // Always be valid for distance, age range, location, notifications, and contacts
       isValid = true;
     }
 
-    if (_currentStep == 5) {
+    if (_currentStep == 3) {
       isValid = ref.read(isVerifiedProvider);
     }
 
