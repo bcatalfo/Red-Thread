e692cb58324be6a2a26ffbc06251fadb0465f9c7
scatalfo <scatalfo@gmail.com>
2024-06-12 23:05:20 -0400

    remove bad import

--- red_thread/lib/presentation/pages/chat.dart
+++ red_thread/lib/presentation/pages/chat.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
